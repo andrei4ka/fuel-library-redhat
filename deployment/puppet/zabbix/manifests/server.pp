f3146e90aa375dd5094828cdaaa924250442e55a
Marcin Iwinski <miwinski@mirantis.com>
2015-04-01 06:32:23 +0200

    enable loose mode reverse filtering on br-ex
    since we do not have public network on ceph nodes and we do have both public
    and storage networks on controllers we need to allow br-ex to reply via other
    interfaces

--- deployment/puppet/zabbix/manifests/server.pp
+++ deployment/puppet/zabbix/manifests/server.pp
@@ -116,5 +116,14 @@ class zabbix::server(
     action    => 'accept',
     port      => $zabbix::ports['backend_server'] ? { unset=>$zabbix::ports['server'], default=>$zabbix::ports['backend_server'] },
   }
+  if !defined(Sysctl::Value['net.ipv4.conf.br-ex.rp_filter']) {
+    sysctl::value { 'net.ipv4.conf.br-ex.rp_filter':
+      value => '2'
+    }
+  }
+  augeas { 'sysctl-net.ipv4.conf.br-ex.rp_filter':
+    context => '/files/etc/sysctl.conf',
+    changes => "set net.ipv4.conf.br-ex.rp_filter '2'",
+  }
 
 }
